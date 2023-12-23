0973ee9cdda9bd0d9aa08c5253c80406cb664f45
Pontus A <pontus.abrahamsson@iths.se>
2023-12-23 08:53:29 +0100

    first commit

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -1,21 +1,39 @@
-import type { GatsbyConfig } from "gatsby";
+import type { GatsbyConfig } from 'gatsby';
+
+require('dotenv').config({
+  path: `.env.${process.env.NODE_ENV}`,
+});
 
 const config: GatsbyConfig = {
   siteMetadata: {
     title: `Portfolio site`,
-    siteUrl: `https://www.yourdomain.tld`
+    siteUrl: `https://www.yourdomain.tld`,
   },
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
-  plugins: [{
-    resolve: 'gatsby-source-contentful',
-    options: {
-      "accessToken": "DLGS7vDtzUhH0myvhZlyHRi6u-M3kkoh2qwdM8AfpQk",
-      "spaceId": "ucpozvn34kfc"
-    }
-  }, "gatsby-plugin-image", "gatsby-plugin-sharp", "gatsby-transformer-sharp", "gatsby-plugin-postcss"]
+  plugins: [
+    {
+      resolve: 'gatsby-source-contentful',
+      options: {
+        accessToken: process.env.ACCESS_TOKEN,
+        spaceId: process.env.SPACE_ID,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+    'gatsby-plugin-image',
+    'gatsby-plugin-sharp',
+    'gatsby-transformer-sharp',
+    'gatsby-plugin-postcss',
+    '@chakra-ui/gatsby-plugin',
+  ],
 };
 
 export default config;
---
+++ src/pages/aboutme.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import Layout from '../components/layout';
+const AboutPage = () => {
+  return (
+    <Layout>
+      <main>
+        <h1>About Page</h1>
+        <p>Welcome to the about page!</p>
+      </main>
+    </Layout>
+  );
+};
+
+export default AboutPage;
